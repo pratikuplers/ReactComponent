7e8bf0bd818a6c43b4eef852e5cdeef5e6bb061c
pratikuplers <pratik.d@uplers.in>
2022-03-10 11:59:44 +0530

    upload files

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import logo from './images/logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  const expense = [
+    {
+      id: '1',
+      title: 'Medical',
+      amount: 660,
+      date: new Date(2022, 1, 1)
+    },
+    {
+      id: '2',
+      title: 'IT',
+      amount: 780,
+      date: new Date(2022, 3, 1)
+    },
+    {
+      id: '3',
+      title: 'Car',
+      amount: 980,
+      date: new Date(2022, 1, 4)
+    }
+  ];
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="App-header">
+        <ExpenseItem 
+            title={expense[0].title} 
+            amount={expense[0].amount} 
+            date={expense[0].date}>
+        </ExpenseItem>
+        <ExpenseItem 
+            title={expense[1].title} 
+            amount={expense[1].amount} 
+            date={expense[1].date}>
+        </ExpenseItem>
+        <ExpenseItem 
+            title={expense[2].title} 
+            amount={expense[2].amount} 
+            date={expense[2].date}>
+        </ExpenseItem>
+      </div>
+    </div>
+  );
+}
+export default App;
