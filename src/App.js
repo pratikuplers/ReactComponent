58fa63e07751f7effe0d3dff73bc19dbfd207c5a
pratikuplers <pratik.d@uplers.in>
2022-03-15 11:59:15 +0530

    import chart

--- src/App.js
+++ src/App.js
@@ -1,47 +1,79 @@
 import logo from './images/logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
 import ExpenseItem from './components/Expenses/ExpenseItem';
+import ExpenseFilter from './components/Expenses/ExpenseFilter';
+import ExpenseChart from './components/Expenses/ExpenseChart';
+const dummyData = [
+  {
+    id: '1',
+    title: 'Medical',
+    amount: 660,
+    date: new Date(2021, 1, 1)
+  },
+  {
+    id: '2',
+    title: 'IT',
+    amount: 780,
+    date: new Date(2018, 3, 1)
+  },
+  {
+    id: '3',
+    title: 'Car',
+    amount: 200,
+    date: new Date(2019, 1, 4)
+  },
+  {
+    id: '4',
+    title: 'Photo',
+    amount: 300,
+    date: new Date(2019, 2, 5)
+  },
+  {
+    id: '5',
+    title: 'Books',
+    amount: 50,
+    date: new Date(2020, 1, 4)
+  },
+];
+const App = (props) => {
+  
+  const [filteredYear, setFilteredYear] = useState('2021');
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
 
-const App = () => {
-  const expense = [
-    {
-      id: '1',
-      title: 'Medical',
-      amount: 660,
-      date: new Date(2022, 1, 1)
-    },
-    {
-      id: '2',
-      title: 'IT',
-      amount: 780,
-      date: new Date(2022, 3, 1)
-    },
-    {
-      id: '3',
-      title: 'Car',
-      amount: 980,
-      date: new Date(2022, 1, 4)
+  const [expense, setNewExpense] = useState(dummyData);
+  const addExpenseHandler = expenses => {
+    setNewExpense((prevExpenses) => {
+      return [expenses, ...prevExpenses];
+    });
+  }
+
+  const filterExpense = expense.filter(
+    expense => {
+      return expense.date.getFullYear().toString() == filteredYear;
     }
-  ];
+  )
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      
+      <ExpenseFilter selected = {filteredYear} onChangeFilter={filterChangeHandler} />
+      <ExpenseChart expense={filterExpense}/>
       <div className="App-header">
+      {filterExpense.length === 0 && <h4>No expenses found here.</h4>}
+      {filterExpense.length > 0 &&
+      filterExpense.map( (data) =>(
         <ExpenseItem 
-            title={expense[0].title} 
-            amount={expense[0].amount} 
-            date={expense[0].date}>
+            key={data.id}
+            title={data.title} 
+            amount={data.amount} 
+            date={data.date}>
         </ExpenseItem>
-        <ExpenseItem 
-            title={expense[1].title} 
-            amount={expense[1].amount} 
-            date={expense[1].date}>
-        </ExpenseItem>
-        <ExpenseItem 
-            title={expense[2].title} 
-            amount={expense[2].amount} 
-            date={expense[2].date}>
-        </ExpenseItem>
+        ))}
       </div>
     </div>
   );
